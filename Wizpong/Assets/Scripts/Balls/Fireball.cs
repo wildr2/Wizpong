f7936b26ebe12ebf851c8b72dbb16fe05364a50f
Robert Wild <wildr2@rpi.edu>
2015-01-17 15:15:49 -0500

    Improve balls

---
+++ Wizpong/Assets/Scripts/Balls/Fireball.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : Ball 
+{
+    // Other references
+    public BallAudio ball_audio;
+    public FireballTrailSegment trail_segment_prefab;
+
+    private float distance_per_segment = 0.5f;
+    private float distance = 0;
+
+
+    new public void Start()
+    {
+        ObjectPool.Instance.RequestObjects(trail_segment_prefab, 60, true);
+
+        base.Start();
+    }
+    public void Update()
+    {
+        distance += rigidbody2D.velocity.magnitude * Time.deltaTime;
+        if (distance > distance_per_segment)
+        {
+            distance = 0;
+
+            // create trail segment
+            FireballTrailSegment segment = ObjectPool.Instance.GetObject(trail_segment_prefab, false);
+            segment.transform.position = transform.position;
+            segment.Initialize(this);
+        }
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Wall"))
+        {
+            ball_audio.PlayBumpSound(rigidbody2D.velocity.magnitude / 50f);
+        }
+        else if (col.collider.CompareTag("Ball"))
+        {
+            ball_audio.PlayBumpSound(rigidbody2D.velocity.magnitude / 50f);
+        }
+    }
+
+    public override void TakeControl(Racquet racquet)
+    {
+        ball_audio.PlayPossessSound();
+        base.TakeControl(racquet);
+    }
+
+    public Racquet ControllingRacquet()
+    {
+        return controlling_racquet;
+    }
+	
+}
---
+++ Wizpong/Assets/Scripts/Balls/FireballTrailSegment.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballTrailSegment : MonoBehaviour
+{
+    private Fireball parent;
+
+    public void Initialize(Fireball parent)
+    {
+        this.parent = parent;
+        StartCoroutine("DeactivateAfterWait");
+    }
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        // VS Racquet physical
+        if (collider.CompareTag("Racquet"))
+        {
+            Racquet r = collider.GetComponent<Racquet>();
+
+            // only stun racquets that are not currently controlling the fireball (have it in the control zone)
+            if (r != null && !(parent.ControllingRacquet() != null && parent.ControllingRacquet().ControlledBall() == parent))
+            {
+                // stun the racquet and move them away from the fire
+                r.Stun(0.2f);
+                Vector2 v = transform.position - r.transform.position;
+                r.transform.Translate(-v * 0.5f);
+            }
+        }
+    }
+
+    private IEnumerator DeactivateAfterWait()
+    {
+        yield return new WaitForSeconds(3);
+
+        gameObject.SetActive(false);
+    }
+}
